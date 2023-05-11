1befc8ac8ec66bd44ec6aed4a9be17fe17d92964
Abhishek Yergude <112966485+abhii019@users.noreply.github.com>
2023-05-11 14:42:46 +0530

    new

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function signIn(e) {
         alert("Incorrect login credentials");
     }
     else{
-        location.href = "vivek.html";
+        location.href = "home.html";
     }
     e.preventDefault();
 }
